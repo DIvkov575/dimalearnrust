a3ddd538895382453c4f0b148b4b394c141a7ca1
Divkov575 <dmitriy@ivkov.net>
2023-02-14 21:08:42 -0500

    i cannot code

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,16 @@
-// created echo
-// fn main() {
-//     let args: Vec<String> = std::env::args().collect();
-//     let mut ctr: u16 = 0;
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::ffi::{OsString};
 
-//     for i in args{
-//         if ctr > 0 {
-//             print!("{} ", i);
-//         }
-//         ctr+=1;
-//     }
-// }
-
-#![feature(option_result_contains)]
 fn main() {
-   //cat
     let args: Vec<String> = std::env::args().collect();
-    // let mut ctr: u16 = 0;
 
-    let mut iter = args.iter();
-        iter.next();
-        loop {
-            let elem = iter.next();
-            if elem.contains(&"/") {
-                println!("inner");
-                // let path = std::path::Path::new(&elem);
-                if let Some(path) = std::path::Path::new(&elem)  {
-                    let file_content: String = std::fs::read_to_string(&path).unwrap();
-                    println!("{}", &file_content)
-                }
+    let path = OsString::from(&args[1]);
+    // println!("{}", path.to_string());
+    let file = File::open(path).unwrap();
+    let lines = io::BufReader::new(file).lines();
+    for line in lines {
+        println!("{}", line.unwrap());
 
-            }
-            // println!("{}", elem.unwrap());
-        }
+    }
 }
