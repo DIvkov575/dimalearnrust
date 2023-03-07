1c73aee173b08956c06da0a2ced0a5517eb659c9
Divkov575 <dmitriy@ivkov.net>
2023-03-07 18:32:02 -0500

    tmp save dijstrika in main

--- src/main.rs
+++ src/main.rs
@@ -1,96 +1,66 @@
-use clap::{self, Parser};
-use regex::{Regex, RegexBuilder};
-use std::collections::LinkedList;
-use std::{
-    error::Error,
-    fs::{self, File},
-    io::{self, BufRead, BufReader},
-    mem,
-};
-use walkdir::WalkDir;
-
-#[derive(Parser, Debug)]
-#[command(author, version, about, long_about = None)]
-struct Args {
-    #[arg(short, long)]
-    pattern: String,
-    #[arg(short, long)]
-    files: Vec<String>,
-    #[arg(short, long)]
-    insensitive: bool,
-    #[arg(short, long)]
-    recursive: bool,
-    #[arg(short, long)]
-    count: bool,
-    #[arg(short, long)]
-    verbose: bool,
+// use std::collections::Queue;
+#[allow(dead_code)]
+struct E {
+    weight: usize,
+    node: usize
 }
 
 fn main() {
-    let args = Args::parse();
-    let pattern = RegexBuilder::new(&args.pattern)
-        .case_insensitive(args.insensitive)
-        .build()
-        .map_err(|_| format!("Invalid pattern \"{}\"", &args.pattern))
-        .unwrap();
-    let mut count = 0;
-    for path_a in args.files {
-        if fs::metadata(&path_a).unwrap().is_file() {
-            let lines: Vec<String> = BufReader::new(File::open(&path_a).unwrap())
-                .lines()
-                .map(|x| x.unwrap())
-                .collect();
+    #[allow(non_snake_case)]
+    let mut V: [i8; 5] = [-1; 5];
+    #[allow(non_snake_case)]
+    let W = std::collections::HashMap::from([
+        (0,Vec::from([(1,4),(2,3)])),
+        (1,Vec::from([(2,5),(3,6)])),
+        (2,Vec::from([(4, 7)])),
+        (3,Vec::from([(5, 3)])),
+        (4,Vec::from([(-1,0)])),
+        (5,Vec::from([(-1,0)]))
+    ]);
 
-            // let mut pat_match_locations: LinkedList<usize> = LinkedList::new();
-            let mut pat_match_locations: Vec<usize> = Vec::new();
-            pat_match_locations.push(0);
-            let content = fs::read_to_string(&path_a).unwrap();
 
-            for pat_match in pattern.captures_iter(&content) {
-                let nl_count: usize = content[0..pat_match.get(0).unwrap().start()]
-                    .matches('\n')
-                    .count();
-                if args.verbose {
-                    println!("{}, {}, line: {}", &path_a, nl_count, lines[nl_count])
-                } else {
-                    println!("{}, line: {}", &path_a, nl_count)
-                }
-                count += 1;
-            }
-        } else {
-            if args.recursive {
-                for path_b in WalkDir::new(&path_a)
-                    .into_iter()
-                    .filter_map(|file| file.ok())
-                {
-                    if path_b.metadata().unwrap().is_file() {
-                        println!("{}", path_b.path().display());
-                        let lines: Vec<String> = BufReader::new(File::open(path_b.path()).unwrap())
-                            .lines()
-                            .map(|x| x.unwrap())
-                            .collect();
-                        let mut pat_match_locations: LinkedList<usize> = LinkedList::new();
-                        pat_match_locations.push_back(0);
-                        let contents = std::fs::read_to_string(path_b.path()).unwrap();
+    let start = 0;
+    // let end = 5;
+    V[start] = 0;
 
-                        for pat_match in pattern.captures_iter(&contents) {
-                            let nl_count = contents[*pat_match_locations.front().unwrap()
-                                ..pat_match.get(0).unwrap().start()]
-                                .matches('\n')
-                                .count();
-                            if args.verbose {
-                                println!("{}, {}, line: {}", &path_a, nl_count, lines[nl_count])
-                            } else {
-                                println!("{}, line: {}", &path_a, nl_count)
-                            }
-                            count += 1;
-                        }
-                    }
-                }
-            }
-        }
+    #[allow(non_snake_case)]
+    let Q: Vec<i8> = W.get(&start).unwrap().iter().map(|x| x.0).collect();
+    let index = min_edge(W.get(&start).unwrap());
+    let weight = W.get(&start).unwrap()[index].1;
+    let next_node: isize = W.get(&start).unwrap()[index].0.into();
+    if next_node == -1 {panic!("end reached");
     }
-    if args.count {
-        println!("Total number of occurrences: {}", count);
+
+    if V[next_node as usize] == -1 {
+        V[next_node as usize] = V[start] + weight as i8;
     }
+
+
+    println!("V: {:#?}", V);
+    println!("Q: {:#?}", Q);
+
 }
+
+fn min_edge(input: &Vec<(i8,u8)>) -> usize {
+    let mut counter:u8 = 0;
+    let mut min_edge:u8 = input[0].1;
+    let mut min_node_index:u8 = 0;
+    for edge in input {
+       if edge.1 < min_edge {
+           min_edge = edge.1;
+           min_node_index = counter;
+       }
+        counter += 1;
+    }
+    min_node_index as usize
+
+}
+    // #[allow(non_snake_case)]
+    // let W = std::collections::HashMap::from([
+    //     (0,Vec::from([1,2])),
+    //     (1,Vec::from([2,3])),
+    //     (2,Vec::from([4])),
+    //     (3,Vec::from([5])),
+    //     (4,Vec::from([-1])),
+    //     (5,Vec::from([-1]))
+    // ]);
