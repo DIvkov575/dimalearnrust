1f524ac111d61ee27f87792cdd54c6cec3d2d121
Divkov575 <dmitriy@ivkov.net>
2023-09-01 23:47:30 -0400

    more dfs

--- graph/dfs/src/lib.rs
+++ graph/dfs/src/lib.rs
@@ -1,8 +1,22 @@
-// setup for recursive dfs implementation
+use std::fmt;
+use std::error::Error;
+
 pub const END: i32 = 6;
 #[derive(Debug)]
 #[allow(dead_code)]
 pub struct E {
-    weight: i32,
-    node: i32,
+    pub weight: i32,
+    pub node: i32,
 }
+
+#[derive(Debug)]
+pub struct MyError(String);
+
+impl fmt::Display for MyError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "There is an error: {}", self.0)
+    }
+}
+
+impl Error for MyError {}
+
--- graph/dfs/src/main.rs
+++ graph/dfs/src/main.rs
@@ -1,12 +1,17 @@
 use std::collections::{HashMap, HashSet};
+use std::error::Error;
+mod lib;
 use lib::{END, E};
 
 
+// fn main() -> Result<(), Box<dyn Error>> {
 fn main() {
-    iterative_dfs();
-    recursive_dfs_wrapper();
+    // iterative_dfs();
+    let path = iterative_dfs_ret_path().unwrap();
+    print!("{:?}", path);
+    // recursive_dfs_wrapper();
+    // Ok(())
 }
-
 
 fn iterative_dfs() {
     let start = 1;
@@ -51,6 +56,62 @@ fn iterative_dfs() {
         println!("current: {}", current_node);
         println!("stack: {:?}", stack);
     }
+}
+
+
+fn iterative_dfs_ret_path() -> Result<Vec<u32>, Box<dyn Error>> {
+    let start = 1;
+    let target = 8;
+
+    #[allow(non_snake_case)]
+        let V: Vec<Vec<u32>> = Vec::from([
+        Vec::from([2, 4, 5]),
+        Vec::from([3]),
+        Vec::from([7, 8]),
+        Vec::from([7]),
+        Vec::from([6, 7]),
+        Vec::from([]),
+        Vec::from([]),
+        Vec::from([]),
+    ]);
+    let mut visited: Vec<u32> = vec![0; V.len()];
+    let mut stack: Vec<u32> = Vec::new();
+    stack.push(start);
+    let mut current_node: usize = 1;
+    let mut path_stack: Vec<u32> = Vec::from([start, start]);
+
+     loop {
+        path_stack.pop();
+        current_node = stack.pop().unwrap() as usize;
+
+        if visited[current_node - 1] == 0 {
+            path_stack.push(current_node as u32);
+            visited[current_node - 1] = 1;
+            let neighbors: &Vec<u32> = &V[current_node - 1];
+
+            // if neighbors.is_empty() {
+            //     // stack.pop();
+            //     // continue
+            // } else i
+            if neighbors.contains(&target) {
+                path_stack.push(target);
+                return Ok(path_stack);
+            } else if !(visited.iter().any(|x| *x == 0 as u32)) {
+                Err("bad erorr")?;
+            } else {
+                stack.splice(stack.len()..stack.len(), neighbors.clone());
+            };
+        } else {
+            path_stack.push(current_node as u32);
+        };
+
+        println!("current: {}", current_node);
+        println!("stack: {:?}", stack);
+        println!("path stack: {:?}", path_stack);
+        println!("visited: {:?}", visited);
+    };
+
+
 }
 
 fn recursive_dfs_wrapper() {
