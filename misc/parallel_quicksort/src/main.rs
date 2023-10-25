4b72561e97dfac2c9c12b2e3358c88cb6bf0f4cc
Divkov575 <dmitriy@ivkov.net>
2023-10-25 12:35:51 -0400

    workign parallelism but slower than sequential

--- misc/parallel_quicksort/src/main.rs
+++ misc/parallel_quicksort/src/main.rs
@@ -11,21 +11,16 @@ fn main() -> Result<(), Box<dyn Error>> {
     let input: Vec<usize> = harr!(10, 42);
     println!("Input: {:?}", input);
 
-    // let mut timer = Instant::now();
-    // let output1 = q_sort(input);
-    // println!("q_sort1: {}", timer.elapsed());
-    // println!("Output1: {:?}", output1);
-
     let mut timer = Instant::now();
+    let output1 = q_sort(input.clone());
+    println!("q_sort1: {:#?}", timer.elapsed());
+    println!("Output1: {:?}", output1);
+
+    timer = Instant::now();
     let output1 = q_sort_parallel(input);
-    println!("q_sort2: {}", timer.elapsed());
+    println!("q_sort2: {:#?}", timer.elapsed());
     println!("Output2: {:?}", output1);
 
-    // let a = thread::spawn(|| {
-    //     return 5
-    // }).join().unwrap();
-    // // a.join().unwrap();
-    // println!("{a}");
 
 
     Ok(())
@@ -34,50 +29,43 @@ fn main() -> Result<(), Box<dyn Error>> {
 
 
 fn q_sort_parallel(input: Vec<usize>) -> Vec<usize> {
-    let len = input.len();
-    let pivot = input[len-1];
-
-    let mut L: Vec<usize>  = Vec::with_capacity(len/2);
+    let mut output: Vec<usize> = Vec::new();
+    let mut L: Vec<usize>  = Vec::with_capacity(input.len() /2);
     let mut M: Vec<usize>  = Vec::with_capacity(1);
-    let mut R: Vec<usize>  = Vec::with_capacity(len/2);
+    let mut R: Vec<usize>  = Vec::with_capacity(input.len() /2);
 
-    for i in 0..len {
-        if input[i]  < pivot {
+    for i in 0..input.len() {
+        if input[i]  < input[input.len() -1] {
             L.push(input[i]);
-        } else if input[i] > pivot {
+        } else if input[i] > input[input.len() -1] {
             R.push(input[i]);
         } else {
             M.push(input[i]);
         }
     }
 
-    // println!("pivot: {:?}", pivot);
-    // println!("Left: {:?}", L);
-    // println!("Middle: {:?}", M);
-    // println!("Right: {:?}", R);
+    if L.len() > 1 && R.len() > 1 {
+        let l_handle = spawn(|| q_sort_parallel(L));
+        let r_handle = spawn(|| q_sort_parallel(R));
 
-    let mut output: Vec<usize> = Vec::new();
-    let l_handle: JoinHandle<_>;
-    let r_handle: JoinHandle<_>;
-
-    if L.len() > 1 { l_handle = spawn(|| q_sort_parallel(&mut L)); }
-    if R.len() > 1 { r_handle = spawn(|| q_sort_parallel(&mut R) ); }
-
-
-    if L.len() >= 1 && R.len() >= 1 {
         output.append(&mut l_handle.join().unwrap());
         output.append(&mut M);
         output.append(&mut r_handle.join().unwrap());
-    }
-
-    if L.len() <= 1 && R.len() <= 1 {
+    } else if L.len() > 1  && R.len() <= 1{
+        output.append(&mut q_sort_parallel(L));
+        output.append(&mut M);
+        output.append(&mut R);
+    } else if L.len() <= 1  && R.len() > 1{
+        output.append(&mut L);
+        output.append(&mut M);
+        output.append(&mut q_sort_parallel(R));
+    } else {
         output.append(&mut L);
         output.append(&mut M);
         output.append(&mut R);
     }
 
     output
-
 }
 
 
