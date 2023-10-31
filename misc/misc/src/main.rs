9fb86ce3adaa52b954339f28e79cd673d3db87fe
Divkov575 <dmitriy@ivkov.net>
2023-10-30 23:05:48 -0400

    messing w/ implementing traits for external stuff

--- misc/misc/src/main.rs
+++ misc/misc/src/main.rs
@@ -1,47 +1,18 @@
+use std::collections::HashMap;
+use anyhow::Error;
+
 fn main() {
-    let mut time;
-    let mut output_times: Vec<_> = Vec::new();
-    let i1 = 500000;
-
-
-    time = std::time::Instant::now();
-    for i in 0..i1 {
-        println!("hello");
+    trait a {
+        fn a(&self) {}
     }
-    output_times.push(time.elapsed());
 
-    //obtaining lock ======================
-    let mut lock = stdout().lock();
-
-    time = std::time::Instant::now();
-    for i in 0..i1 {
-        write!(&mut lock, "hello").unwrap();
+    impl a for anyhow::Error {
+        fn a(&self) {
+            print!("AAAAAAAAAAAAAAAAa");
+        }
     }
-    output_times.push(time.elapsed());
 
-
-
-    time = std::time::Instant::now();
-    for i in 0..i1 {
-        println!("hello");
-    }
-    output_times.push(time.elapsed());
-
-    time = std::time::Instant::now();
-    for i in 0..i1 {
-        print!("hello \n");
-    }
-    output_times.push(time.elapsed());
-
-    time = std::time::Instant::now();
-    for i in 0..i1 {
-        print!("hello  ");
-    }
-    output_times.push(time.elapsed());
-
-
-
-
-
-    println!("\n {:?}", output_times);
+    let inner = String::from("asdg").into();
+    let b = anyhow::Error{inner};
+    b.a();
 }
