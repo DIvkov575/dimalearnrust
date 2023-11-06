92a7336db45c56ef073f7d048a8f6634882bcccf
Divkov575 <dmitriy@ivkov.net>
2023-11-06 11:20:36 -0500

    working parallel_partition & else

--- misc/parallel_quicksort/src/main.rs
+++ misc/parallel_quicksort/src/main.rs
@@ -9,103 +9,84 @@ use std::error::Error;
 use std::thread::{spawn, JoinHandle};
 use std::sync::Arc;
 use std::time::{Duration, Instant};
+use itertools::Itertools;
+use rayon::prelude::*;
 
 
 fn main() -> Result<(), Box<dyn Error>> {
     let mut timer;
-    let input: Vec<usize> = harr!(1*(10usize).pow(7), 42);
+    let input_a: Vec<usize> = harr!(1*(10usize).pow(5), 42);
+    let input_b = input_a.clone();
+    let input_c= input_a.clone();
     let pool = rayon::ThreadPoolBuilder::new().num_threads(8).build().unwrap();
 
+    // comparison of various sorting algos
+    // entirely serial
+    timer = Instant::now();
+    let output1 = q_sort(input_a);
+    let t1 = timer.elapsed();
+    // serial partition
+    timer = Instant::now();
+    let output1 = pool.install(|| q_sort_parallel1(input_b));
+    let t2 = timer.elapsed();
+    // sequential partition
+    timer = Instant::now();
+    let output2 = pool.install(|| q_sort_parallel2(input_c));
+    let t3 = timer.elapsed();
+
+
+    println!("Output1: {:.3?}", t1);
+    println!("Output2: {:.3?}", t2);
+    println!("Output3: {:.3?}", t3);
+
+
+
+    // comparison of serial vs parallel partition
 
     // timer = Instant::now();
-    // let output1 = q_sort(input.clone());
-    // let t1 = timer.elapsed();
-    // println!("Output1: {:.3?}", t1);
+    // let pivot = input[input.len()/2];
+    // let mut output: Vec<usize> = Vec::with_capacity(input.len());
+    // let mut L: Vec<usize> = Vec::with_capacity(input.len() / 2);
+    // let mut M: Vec<usize> = Vec::with_capacity(1);
+    // let mut R: Vec<usize> = Vec::with_capacity(input.len() / 2);
+    // for i in 0..input.len() {
+    //     if input[i] < pivot { L.push(input[i]);
+    //     } else if input[i] > pivot { R.push(input[i]);
+    //     } else { M.push(input[i]); }
+    // }
+    // println!("Output2: {:.3?}", timer.elapsed());
 
     // timer = Instant::now();
-    // let output1 = pool.install(|| q_sort_parallel2(input));
-    // let t2 = timer.elapsed();
-    // println!("Output2: {:.3?}", t2);
-
-    timer = Instant::now();
-    let pivot = input[input.len()/2];
-    let mut output: Vec<usize> = Vec::with_capacity(input.len());
-    let mut L: Vec<usize> = Vec::with_capacity(input.len() / 2);
-    let mut M: Vec<usize> = Vec::with_capacity(1);
-    let mut R: Vec<usize> = Vec::with_capacity(input.len() / 2);
-    for i in 0..input.len() {
-        if input[i] < pivot {
-            L.push(input[i]);
-        } else if input[i] > pivot {
-            R.push(input[i]);
-        } else {
-            M.push(input[i]);
-        }
-    }
-    println!("Output2: {:.3?}", timer.elapsed());
+    // let (_, _, _) = serialized_partition(pivot, &input);
+    // println!("Output2: {:.3?}", timer.elapsed());
 
 
-
-    timer = Instant::now();
-    let (_, _, _) = serialized_partition(pivot, &input);
-    println!("Output2: {:.3?}", timer.elapsed());
-
-
-    // println!("Dif: {:?}", t1.as_millis() as f64 /t2.as_millis() as f64 );
     Ok(())
 }
 
-fn serialized_partition(pivot: usize, input: &Vec<usize>) -> (Vec<usize>, Vec<usize>, Vec<usize>){
+fn parallel_partition(pivot: usize, input: &Vec<usize>) -> (Vec<usize>, Vec<usize>, Vec<usize>){
     let mut L: Vec<usize> = Vec::new();
     let mut M: Vec<usize> = Vec::new();
     let mut R: Vec<usize> = Vec::new();
-    let split: usize = input.len()/2;
-    let ( (
-            mut L1,
-            mut M1,
-            mut R1),
-        (
-            mut L2,
-            mut M2,
-            mut R2)
-    ) = rayon::join(|| {
-                        let mut L: Vec<usize> = Vec::new();
-                        let mut M: Vec<usize> = Vec::new();
-                        let mut R: Vec<usize> = Vec::new();
-                        for i in 0..split{
-                            if input[i] < pivot {
-                                L.push(input[i]);
-                            } else if input[i] > pivot{
-                                R.push(input[i]);
-                            } else {
-                                M.push(input[i]);
-                            }
-                        }
-                    return (L, M, R);
-                    },
-                || {
-                    let mut L: Vec<usize> = Vec::new();
-                    let mut M: Vec<usize> = Vec::new();
-                    let mut R: Vec<usize> = Vec::new();
-                    for i in split..input.len(){
-                        if input[i] < pivot {
-                            L.push(input[i]);
-                        } else if input[i] > pivot{
-                            R.push(input[i]);
-                        } else {
-                            M.push(input[i]);
-                        }
-                    }
-                    return (L, M, R);
-                }
-    );
+    let split_num = 4;
+    let split_size = input.len()/split_num;
 
-    L.append(&mut L1);
-    L.append(&mut L2);
-    M.append(&mut M1);
-    M.append(&mut M2);
-    R.append(&mut R1);
-    R.append(&mut R2);
+    let chunks: Vec<Vec<usize>> = input.chunks(split_size).map(|x| x.to_vec()).collect();
+    chunks.par_iter().map( |chunk| {
+        let mut L: Vec<usize> = Vec::new();
+        let mut M: Vec<usize> = Vec::new();
+        let mut R: Vec<usize> = Vec::new();
+        for i in 0..chunk.len() {
+            if chunk[i] < pivot {
+                L.push(chunk[i]);
+            } else if chunk[i] > pivot {
+                R.push(chunk[i]);
+            } else {
+                M.push(chunk[i]);
+            }
+        }
+        return (L, M, R);
+    } );
 
     return (L, M, R)
 }
@@ -117,17 +98,7 @@ fn q_sort_parallel2(input: Vec<usize>) -> Vec<usize> {
     let mut M: Vec<usize> = Vec::with_capacity(1);
     let mut R: Vec<usize> = Vec::with_capacity(input.len() / 2);
 
-    (L, M, R) = serialized_partition(pivot, &input);
-
-    // for i in 0..input.len() {
-    //     if input[i] < pivot {
-    //         L.push(input[i]);
-    //     } else if input[i] > pivot {
-    //         R.push(input[i]);
-    //     } else {
-    //         M.push(input[i]);
-    //     }
-    // }
+    (L, M, R) = parallel_partition(pivot, &input);
 
     if L.len() > 1 && R.len() > 1 {
         let (mut l_handle, mut r_handle) = rayon::join(
@@ -157,7 +128,7 @@ fn q_sort_parallel2(input: Vec<usize>) -> Vec<usize> {
 
 
 
-fn q_sort_parallel1(input: Vec<usize>, pool: Arc<rayon::ThreadPool>) -> Vec<usize> {
+fn q_sort_parallel1(input: Vec<usize>) -> Vec<usize> {
     let pivot = input[input.len()/2];
     let mut output: Vec<usize> = Vec::with_capacity(input.len());
     let mut L: Vec<usize> = Vec::with_capacity(input.len() / 2);
@@ -176,21 +147,21 @@ fn q_sort_parallel1(input: Vec<usize>, pool: Arc<rayon::ThreadPool>) -> Vec<usiz
 
     if L.len() > 1 && R.len() > 1 {
         let (mut l_handle, mut r_handle) = rayon::join(
-            || q_sort_parallel1(L, pool.clone()),
-            || q_sort_parallel1(R, pool.clone())
+            || q_sort_parallel1(L),
+            || q_sort_parallel1(R)
         );
 
         output.append(&mut l_handle);
         output.append(&mut M);
         output.append(&mut r_handle);
     } else if L.len() > 1 && R.len() <= 1 {
-        output.append(&mut q_sort_parallel1(L, pool));
+        output.append(&mut q_sort_parallel1(L));
         output.append(&mut M);
         output.append(&mut R);
     } else if L.len() <= 1 && R.len() > 1 {
         output.append(&mut L);
         output.append(&mut M);
-        output.append(&mut q_sort_parallel1(R, pool));
+        output.append(&mut q_sort_parallel1(R));
     } else {
         output.append(&mut L);
         output.append(&mut M);
