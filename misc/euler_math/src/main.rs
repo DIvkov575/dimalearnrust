d242575920d68c22ca0c3464e39d80151f739f6d
Divkov575 <dmitriy@ivkov.net>
2024-02-08 16:21:57 -0500

    did some math hw

--- misc/euler_math/src/main.rs
+++ misc/euler_math/src/main.rs
@@ -1,23 +1,4 @@
 
-
-fn main() {
-    let mut steps = 3usize;
-    let mut step_size= 0.1;
-
-    let mut x_0 = 1f64;
-    let mut y_0 = 2f64;
-    // let mut tmp;
-
-
-    for i in 0..steps {
-        y_0 += g!(y_0, x_0)*step_size;
-        x_0 += step_size
-    }
-
-    println!("{}", y_0);
-}
-
-
 #[macro_export]
 macro_rules! f {
     ($a:ident) => {($a-1.0)};
@@ -27,3 +8,27 @@ macro_rules! f {
 macro_rules! g {
     ($a:ident, $y:ident) => {($a-$y)};
 }
+#[macro_export]
+macro_rules! h {
+    ($x:ident) => {(2.0-$x)};
+}
+
+fn main() {
+    let steps = 3usize;
+    let step_size= -0.1;
+
+    let mut x_0: f64 = 2.into();
+    let mut y_0: f64 = 1.into();
+    // let mut tmp;
+
+
+    for i in 0..steps {
+        y_0 += h!(x_0)*step_size; // 3
+        // y_0 += g!(x_0, y_0)*step_size; //4
+        x_0 += step_size
+    }
+
+    println!("{}", y_0);
+}
+
+
