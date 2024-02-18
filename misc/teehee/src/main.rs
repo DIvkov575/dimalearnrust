093c129ec069abf60a574da39a4a6807330ede2b
Divkov575 <dmitriy@ivkov.net>
2024-02-18 13:18:50 -0500

    simplified teehee

--- misc/teehee/src/main.rs
+++ misc/teehee/src/main.rs
@@ -5,33 +5,19 @@ use thiserror::{Error, self};
 use std::process::Command;
 
 fn main() -> Result<()> {
-    let args: Vec<String> = std::env::args().collect();
-    // let path = Path::new(&args[1]);
     let path = Path::new("tmp");
-
-    if !path.exists() {
-        std::fs::create_dir(path)?;
-    } else if path.is_file() {
-        return Err(MyError::PathWasFile.into());
-    }
+    if path.exists() { panic!("file 'tmp' already exists") }
 
     let mut file = std::fs::OpenOptions::new().write(true).append(true).create(true).open(path.join("tmp"))?;
-    file.write_all("teststring".as_bytes())?;
+    file.write_all("A".as_bytes())?;
 
     Command::new("git").args(["add", "-A"]).output()?;
     Command::new("git").args(["commit", "-am", r#""test""#] ).output()?;
     Command::new("git").args(["push"]).output()?;
-
     Command::new("git").args(["reset", "HEAD@{1}"]).output()?;
     Command::new("git").args(["push", "--force"]).output()?;
 
+    if path.exists() { std::fs::remove_file(&path)?; }
+
     Ok(())
-
-}
-
-#[derive(Error, Debug)]
-#[error("Dataflow Error")]
-enum MyError {
-    #[error("Path provided was a file")]
-    PathWasFile
 }
