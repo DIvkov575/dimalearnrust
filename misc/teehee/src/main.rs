f623c90d1ebab51867a2a41fe6dae3afdcad50bc
Divkov575 <dmitriy@ivkov.net>
2024-02-17 23:36:09 -0500

    updates to teehee

--- misc/teehee/src/main.rs
+++ misc/teehee/src/main.rs
@@ -1,26 +1,33 @@
+use std::io::Write;
+use std::path::Path;
 use anyhow::Result;
-use thiserror::Error;
+use thiserror::{Error, self};
+use std::process::Command;
 
 fn main() -> Result<()> {
-    let args: Vec<String> = std::env::args();
-    let path = std::fs::Path::from(jargs[1]); // ensure exists
+    let args: Vec<String> = std::env::args().collect();
+    let path = Path::new(&args[1]);
 
     if !path.exists() {
-        std::fs::create_dir(path)
+        std::fs::create_dir(path)?;
     } else if path.is_file() {
-        return Err(MyError::PathWasFile.into())
+        return Err(MyError::PathWasFile.into());
     }
 
-    let file = std::fs::OpenOptions::new().write(true).create(true).open(path.join("tmp"))?;
+    let mut file = std::fs::OpenOptions::new().write(true).create(true).open(path.join("tmp"))?;
     file.write_all("teststring".as_bytes())?;
 
+    Command::new("git").args(["add", "-A"]).output()?;
     Command::new("git").args(["commit", "-am", r#""test""#] ).output()?;
     Command::new("git").args(["push"]).output()?;
+
+    Ok(())
 
 }
 
 #[derive(Error, Debug)]
+#[error("Dataflow Error")]
 enum MyError {
-    #[Error("Path provided was a file")]
+    #[error("Path provided was a file")]
     PathWasFile
 }
