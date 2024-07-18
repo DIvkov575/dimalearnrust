e5ee818dc24061be49579bd9b2eaabda516463b6
Divkov575 <dmitriy@ivkov.net>
2024-07-17 22:08:30 -0400

    addex complex-num-experiments

---
+++ misc/complex-num-experiments/src/main.rs
@@ -0,0 +1,25 @@
+use std::collections::HashSet;
+fn main() {
+    // let mut map: HashSet<i32> = HashSet::with_capacity(1000);
+    //
+    // for i in 1..=1 {
+    //     for j in 1..50 {
+    //         let a = num::complex::Complex::new(i, j);
+    //         let b = a.conj();
+    //         let c = (a * b).re;
+    //
+    //         println!("{} * {} = {}", a, b, c);
+    //
+    //         if map.contains(&c) {
+    //             println!("collision");
+    //         } else {
+    //             // println!("insertion");
+    //             map.insert(c);
+    //         }
+    //     }
+    // }
+
+    let a = num::
+
+}
+
